6a0a04b978a60329a48f8f3d131558dbb22888af
Keisha Gittens <keishamsa@me.com>
2022-02-09 13:12:53 -0600

    Add views for movie list & detail

--- movies_django/movies/views.py
+++ movies_django/movies/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
-
+from rest_framework import generics
 # Create your views here.
+from .models import Movie
+# Import serializers
+from .serializers import MovieSerializer
+# Create your views here.
+
+# api/movies/
+# INDEX, POST
+class MovieList(generics.ListCreateAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+
+# api/movies/id
+# SHOW, PUT, DELETE
+class MovieDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
