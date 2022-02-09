787c401b9dcfee141ee33367b0c4a988e4b64b8b
rayhaynes504 <twotone504@gmail.com>
2022-02-09 10:55:07 -0600

    Add movie model

--- movies_django/movies/models.py
+++ movies_django/movies/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    genre = models.CharField(max_length=100)
+    release_date = models.DateField()
+    movie_poster = models.URLField()
+
+    def __str__(self):
+        return self.title
